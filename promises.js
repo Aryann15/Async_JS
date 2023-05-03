6f787bc69fca37ae7676bcbf523b048d4a8caaaa
Aryann15 <rajlysm15@gmail.com>
2023-05-03 23:35:56 +0530

    using promises for setTimeouts

--- promises.js
+++ promises.js
@@ -2,20 +2,32 @@
 //just like a real life promise
 //syntax 
 
-const promise = new Promise ((resolve,reject)=>{
-    const sum =1+1
-    if (sum === 2)
-    {resolve('success')
-    }else {
-        reject('Error')
-    }
+// const promise = new Promise ((resolve,reject)=>{
+//     const sum =1+1
+//     if (sum === 2)
+//     {resolve('success')
+//     }else {
+//         reject('Error')
+//     }
+// })
+
+
+// promise.then(message=>{
+//     console.log(message)
+// }).catch(message =>{
+//     console.error(message)
+// })
+
+// resolve and reject are functions
+
+//Async now
+
+setTimeoutPromise(2000).then(()=>{
+console.log("here")
 })
 
-
-promise.then(message=>{
-    console.log(message)
-}).catch(message =>{
-    console.error(message)
-})
-
-//resolve and reject are functions
+function setTimeoutPromise(duration){
+    return new Promise((resolve,reject) => {
+        setTimeout(resolve,duration)
+    })
+}
